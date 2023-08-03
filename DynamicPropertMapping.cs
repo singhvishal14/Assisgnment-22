dd2e26752ce5a2d608f0279d702ba30ee608bdf6
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-08-03 15:38:26 +0530

    initial commit

---
+++ DynamicPropertMapping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment_22
+{
+    public class DynamicPropertMapping
+    {
+        public static void MapProperty(Source source, Destination destination)
+        {
+            Type sourceType = source.GetType();
+            Type destType = destination.GetType();
+            PropertyInfo[] sourceProperties = sourceType.GetProperties();
+            PropertyInfo[] destProperties = destType.GetProperties();
+            foreach (var sourceProp in sourceProperties)
+            {
+                PropertyInfo destinationProp = Array.Find(destProperties, prop => prop.Name == sourceProp.Name);
+                if (destinationProp != null && destinationProp.PropertyType == sourceProp.PropertyType)
+                {
+                    destinationProp.SetValue(destination, sourceProp.GetValue(source));
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment_22
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Source source = new Source()
+                {
+                    Id = 101,
+                    Name = "Tarun Sharma",
+                    Age = 30,
+                    Address = "Chandni Chowk",
+                    City = "Delhi"
+
+                };
+
+                Destination destination = new Destination();
+
+
+           
+
+            // Map properties from the source to destination
+                DynamicPropertMapping.MapProperty(source, destination);
+
+            // Display the values of properties in the Destination class to ensure the mapping was successful
+                Console.WriteLine($"Destination Id: {destination.Id}");
+                Console.WriteLine($"Destination Name: {destination.Name}");
+                Console.WriteLine($"Destination Age: {destination.Age}");
+                Console.WriteLine($"Destination City: {destination.City}"); // Additional property in Destination class
+                Console.ReadKey();
+        }
+        }
+    }
